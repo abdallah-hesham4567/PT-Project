902289a1d43b3aaf796fab6a040196ed1ebbe8ac
14712024100173@stud.cu.edu.eg <14712024100173@stud.cu.edu.eg>
2025-12-04 16:50:16 +0200

    b3d basha

--- ApplicationManager.cpp
+++ ApplicationManager.cpp
@@ -9,16 +9,16 @@ ApplicationManager::ApplicationManager()
 	//Create Input and output
 	pOut = new Output;
 	pIn = pOut->CreateInput();
-	
+
 	StatCount = 0;
 	ConnCount = 0;
 	pSelectedStat = NULL;	//no Statement is selected yet
 	pClipboard = NULL;
-	
+
 	//Create an array of Statement pointers and set them to NULL		
-	for(int i=0; i<MaxCount; i++)
+	for (int i = 0; i < MaxCount; i++)
 	{
-		StatList[i] = NULL;	
+		StatList[i] = NULL;
 		ConnList[i] = NULL;
 	}
 }
@@ -32,42 +32,42 @@ ApplicationManager::ApplicationManager()
 ActionType ApplicationManager::GetUserAction() const
 {
 	//Ask the input to get the action from the user.
-	return pIn->GetUserAction();		
+	return pIn->GetUserAction();
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //Creates an action and executes it
-void ApplicationManager::ExecuteAction(ActionType ActType) 
+void ApplicationManager::ExecuteAction(ActionType ActType)
 {
 	Action* pAct = NULL;
-	
+
 	//According to ActioType, create the corresponding action object
 	switch (ActType)
 	{
-		case ADD_VALUE_ASSIGN:
-			pAct = new AddValueAssign(this);
-			break;
+	case ADD_VALUE_ASSIGN:
+		pAct = new AddValueAssign(this);
+		break;
 
-		case ADD_CONDITION:
-			///create AddCondition Action here
+	case ADD_CONDITION:
+		///create AddCondition Action here
 
-			break;
+		break;
 
-		case SELECT:
-			///create Select Action here
+	case SELECT:
+		///create Select Action here
 
-			break;
+		break;
 
-		case EXIT:
-			///create Exit Action here
-			
-			break;
-		
-		case STATUS:
-			return;
+	case EXIT:
+		///create Exit Action here
+
+		break;
+
+	case STATUS:
+		return;
 	}
-	
+
 	//Execute the created action
-	if(pAct != NULL)
+	if (pAct != NULL)
 	{
 		pAct->Execute();//Execute
 		delete pAct;	//Action is not needed any more ==> delete it
@@ -82,15 +82,15 @@ void ApplicationManager::ExecuteAction(ActionType ActType)
 
 
 //Add a statement to the list of statements
-void ApplicationManager::AddStatement(Statement *pStat)
+void ApplicationManager::AddStatement(Statement* pStat)
 {
-	if(StatCount < MaxCount)
+	if (StatCount < MaxCount)
 		StatList[StatCount++] = pStat;
-	
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////
-Statement *ApplicationManager::GetStatement(Point P) const
+Statement* ApplicationManager::GetStatement(Point P) const
 {
 	//If this point P(x,y) belongs to a statement return a pointer to it.
 	//otherwise, return NULL
@@ -103,23 +103,31 @@ Statement *ApplicationManager::GetStatement(Point P) const
 }
 ////////////////////////////////////////////////////////////////////////////////////
 //Returns the selected statement
-Statement *ApplicationManager::GetSelectedStatement() const
-{	return pSelectedStat;	}
+Statement* ApplicationManager::GetSelectedStatement() const
+{
+	return pSelectedStat;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Set the statement selected by the user
-void ApplicationManager::SetSelectedStatement(Statement *pStat)
-{	pSelectedStat = pStat;	}
+void ApplicationManager::SetSelectedStatement(Statement* pStat)
+{
+	pSelectedStat = pStat;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Returns the Clipboard
-Statement *ApplicationManager::GetClipboard() const
-{	return pClipboard;	}
+Statement* ApplicationManager::GetClipboard() const
+{
+	return pClipboard;
+}
 
 ////////////////////////////////////////////////////////////////////////////////////
 //Set the Clipboard
-void ApplicationManager::SetClipboard(Statement *pStat)
-{	pClipboard = pStat;	}
+void ApplicationManager::SetClipboard(Statement* pStat)
+{
+	pClipboard = pStat;
+}
 
 
 //==================================================================================//
@@ -133,32 +141,149 @@ void ApplicationManager::UpdateInterface() const
 	pOut->ClearDrawArea();
 
 	//Draw all statements
-	for(int i=0; i<StatCount; i++)
+	for (int i = 0; i < StatCount; i++)
 		StatList[i]->Draw(pOut);
-	
+
 	//Draw all connections
-	for(int i=0; i<ConnCount; i++)
+	for (int i = 0; i < ConnCount; i++)
 		ConnList[i]->Draw(pOut);
 
 }
+
+///		////////////////////////////////////////Validation Function///////////////////////////////////////////	
+bool ApplicationManager::Validate() const
+{
+
+	// ==============================================
+	///		 Ensure single Start and End         
+	// ==============================================
+	int startCount = 0;
+	int endCount = 0;
+
+	// Loop through all statements
+	for (int i = 0; i < StatCount; i++)
+	{
+		// Check if this statement is StartEnd
+
+		StartEnd* SE = dynamic_cast<StartEnd*>(StatList[i]);
+		if (SE)
+		{
+			if (SE->IsStart())
+				startCount++;
+			else
+				endCount++;
+		}
+	}
+
+	// Check results
+	if (startCount != 1)
+	{
+		pOut->PrintMessage("Error: Flowchart must have exactly ONE Start Statement.");
+		return false;
+	}
+
+	if (endCount != 1)
+	{
+		pOut->PrintMessage("Error: Flowchart must have exactly ONE End Statement.");
+		return false;
+	}
+
+	// ==============================================
+	///		 Validate outgoing connectors            
+	// ==============================================
+
+	for (int i = 0; i < StatCount; i++)
+	{
+		Statement* S = StatList[i];
+
+		// 1) Check for Conditional statement (needs 2 connectors)
+		Conditional* C = dynamic_cast<Conditional*>(S);
+		if (C)
+		{
+			if (!(C->GetYesConn()) || !(C->GetNoConn()))
+			{
+				pOut->PrintMessage("Error: Conditional statement must have YES and NO connectors.");
+				return false;
+			}
+
+			if (C->GetYesConn()->GetDstStat() == nullptr || C->GetNoConn()->GetDstStat() == nullptr) //cheking that both connectors have destination
+			{
+				pOut->PrintMessage("Error: Conditional connector has no destination.");
+				return false;
+			}
+			continue;
+		}
+
+		// 2) Check for Start (needs 1 connector)
+		StartEnd* SE = dynamic_cast<StartEnd*>(S);
+		if (SE && SE->IsStart())
+		{
+			if (!(SE->GetConnector()))
+			{
+				pOut->PrintMessage("Error: Start must have exactly one outgoing connector.");
+				return false;
+			}
+			if (SE->GetConnector()->GetDstStat() == nullptr) //checking that the start connector has destination
+			{
+				pOut->PrintMessage("Error: Start connector has no destination.");
+				return false;
+			}
+
+			continue;
+		}
+
+		// 3) Check for End (must have 0 connectors)
+		if (SE && !SE->IsStart())
+		{
+			// End must NOT have connector
+			if (SE->GetConnector())
+			{
+				pOut->PrintMessage("Error: End must NOT have an outgoing connector.");
+				return false;
+			}
+			continue;
+		}
+
+		// 4) Normal statements (Assignment, Read, Write): must have 1 connector
+		if (!(S->GetOutConn()))
+		{
+			pOut->PrintMessage("Error: Statement is missing an outgoing connector.");
+			return false;
+
+		}
+		if (S->GetOutConn()->GetDstStat() == nullptr) //checking that the connector has destination
+		{
+			pOut->PrintMessage("Error: Connector has no destination.");
+			return false;
+		}
+	}
+
+
+	return true;
+}
+
 ////////////////////////////////////////////////////////////////////////////////////
 //Return a pointer to the input
-Input *ApplicationManager::GetInput() const
-{	return pIn; }
+Input* ApplicationManager::GetInput() const
+{
+	return pIn;
+}
 //Return a pointer to the output
-Output *ApplicationManager::GetOutput() const
-{	return pOut; }
+Output* ApplicationManager::GetOutput() const
+{
+	return pOut;
+}
 ////////////////////////////////////////////////////////////////////////////////////
 
 
 //Destructor
 ApplicationManager::~ApplicationManager()
 {
-	for(int i=0; i<StatCount; i++)
+	for (int i = 0; i < StatCount; i++)
 		delete StatList[i];
-	for(int i=0; i<StatCount; i++)
+	for (int i = 0; i < ConnCount; i++)
 		delete ConnList[i];
 	delete pIn;
 	delete pOut;
-	
+
 }
--- ApplicationManager.h
+++ ApplicationManager.h
@@ -19,45 +19,50 @@ private:
 	Statement* StatList[MaxCount];	//List of all statements (Array of pointers)
 	Connector* ConnList[MaxCount];	//List of all connectors (Array of pointers)
 
-	Statement *pSelectedStat; //a pointer to the last selected statement
-	                          //you can set and get this pointer
-	Statement *pClipboard;    //a pointer to the last copied/cut statement
-	                          //you can set and get this pointer
+	Statement* pSelectedStat; //a pointer to the last selected statement
+	//you can set and get this pointer
+	Statement* pClipboard;    //a pointer to the last copied/cut statement
+	//you can set and get this pointer
 
-	//Pointers to Input and Output classes
-	Input *pIn;
-	Output *pOut;
+//Pointers to Input and Output classes
+	Input* pIn;
+	Output* pOut;
 
-public:	
-	ApplicationManager(); 
+public:
+	ApplicationManager();
 	~ApplicationManager();
-	
+
 	// == Actions Related Functions ==
 	ActionType GetUserAction() const; //Reads the input command from the user 
-	                                  //and returns the corresponding action type
-	void ExecuteAction(ActionType) ;  //Creates an action and executes it
-	
+	//and returns the corresponding action type
+	void ExecuteAction(ActionType);  //Creates an action and executes it
+
 	// == Statements/Connector Management Functions ==
 	void AddStatement(Statement* pStat);    //Adds a new Statement to the Flowchart
-	Statement *GetStatement(Point P) const;	//Searches for a statement where point P belongs
-	                                        //TODO: Complete its implementation 
-	                                        //      WITHOUT breaking class responsibilities
+	Statement* GetStatement(Point P) const;	//Searches for a statement where point P belongs
+	//TODO: Complete its implementation 
+	//      WITHOUT breaking class responsibilities
 
 	void AddConnector(Connector* pConn);    //Adds a new Connector to the Flowchart
-	Connector *GetConnector(Point P) const;	//search for a Connector where point P belongs
+	Connector* GetConnector(Point P) const;	//search for a Connector where point P belongs
 
 	// Note: you should use the following 4 functions 
 	//       in order not to break class responsibilities (especially in copy, cut and paste)
-	Statement *GetSelectedStatement() const;	 //Returns the selected Statement
-	void SetSelectedStatement(Statement *pStat); //Set the Statement selected by the user
-	Statement *GetClipboard() const;	         //Returns the Clipboard
-	void SetClipboard(Statement *pStat);         //Set the Clipboard
+	Statement* GetSelectedStatement() const;	 //Returns the selected Statement
+	void SetSelectedStatement(Statement* pStat); //Set the Statement selected by the user
+	Statement* GetClipboard() const;	         //Returns the Clipboard
+	void SetClipboard(Statement* pStat);         //Set the Clipboard
 
 	// == Interface Management Functions ==
-	Input *GetInput() const;        //Return pointer to the input
-	Output *GetOutput() const;      //Return pointer to the output
+	Input* GetInput() const;        //Return pointer to the input
+	Output* GetOutput() const;      //Return pointer to the output
 	void UpdateInterface() const;	//Redraws all the drawing window
-	
+
+
+	// == Simulation Functions ==
+	bool Validate() const;
+	void RunSimulation();
+
 };
 
 #endif
