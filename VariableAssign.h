ae1a6023099ab0a7e27b0f81358ee0c00d8bdefd
14712024100173@stud.cu.edu.eg <14712024100173@stud.cu.edu.eg>
2025-12-04 20:16:14 +0200

    VariableAssign Done

---
+++ VariableAssign.h
@@ -0,0 +1,30 @@
+#ifndef Variable_ASSIGN_H
+#define Variable_ASSIGN_H
+#include "Statement.h"
+//Variable Assignment statement class
+//The variable assignment statement assigns a variable to another variable
+class VariableAssign : public Statement
+{
+private:
+	string LHS;	//Left Handside of the assignment (name of a variable)
+	string RHS;	//Right Handside (name of a variable)
+	Connector* pOutConn;	//Variable Assignment Stat. has one Connector to next statement
+	//Each statement type in flowchart has a predefined number of (output) connectors
+	//For example, conditional statement always has 2 output connectors
+	//Note: We don't need to keep track with input connectors
+	//      Whenever we want to iterate on all statements of the flowchart
+	//      we will begin with start statement then its output connector
+	//      then the connector's destination statement and so on (follow the connectors)
+	Point Inlet;	//A point where connections enters this statement 
+	//It's used as the (End) point of the (Input) connectors
+	Point Outlet;	//A point a connection leaves this statement
+	//It's used as the (Start) point of the (Output) connector
+	Point LeftCorner;	//left corenr of the statement block.
+	virtual void UpdateStatementText();
+public:
+	VariableAssign(Point Lcorner, string LeftHS = "", string RightHS = "");
+	void setLHS(const string& L);
+	void setRHS(const string& R);
+	virtual void Draw(Output* pOut) const;
+};
+#endif
