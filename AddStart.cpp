76f8b6228fe385deab4e7a55be60aa21add3d7a0
Abdallah Hesham <abdallahhesham.ce@gmail.com>
2025-12-09 14:37:09 +0200

    Class Addition

---
+++ AddStart.cpp
@@ -0,0 +1,1 @@
+#include "AddStart.h"
--- Delete.cpp
+++ Delete.cpp
@@ -2,6 +2,7 @@
 #include "..\ApplicationManager.h"
 #include "..\GUI\Input.h"
 #include "..\GUI\Output.h"
+#include "..\Statements\Statement.h"
 
 Delete::Delete(ApplicationManager* pAppManager) : Action(pAppManager)
 {
@@ -9,9 +10,30 @@ Delete::Delete(ApplicationManager* pAppManager) : Action(pAppManager)
 
 void Delete::ReadActionParameters()
 {
-	// No parameters to read for delete action
-	Input* pIn = pManager->GetInput();
+	Output* pOut = pManager->GetOutput();
+	pOut->PrintMessage("Delete: ready to delete the selected statement...");
+}
+void Delete::Execute()
+{
+	ReadActionParameters();
 	Output* pOut = pManager->GetOutput();
 
-	pOut->PrintMessage("Delete: Deleting the selected item...");
+	Statement* pState = pManager->GetSelectedStatement();
+
+	if (pState == nullptr)
+	{
+		// No item selected to delete
+		pOut->PrintMessage("Delete: No statement selected to delete.");
+		return;
+	}
+	// Delete the selected statement
+	pManager->RemoveStatement(pState);      //need to add remove statement in app manager
+	// delete the statement from memory
+	delete pState;
+	// Clear the selection
+	pManager->SetSelectedStatement(nullptr);
+	// Update the interface
+	pManager->UpdateInterface();
+
+	pOut->PrintMessage("Delete: Selected statement deleted.");
 }
